0cf6d27a96f5bfc11139be086e758f66f1577116
KimberlySalazarB <118402135+KimberlySalazarB@users.noreply.github.com>
2024-09-27 17:11:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -101,8 +101,7 @@ initial_state = [
     {"role": "system", "content": "You are SazónBot. A friendly assistant helping customers with their lunch orders."},
     {
         "role": "assistant",
-        "content": "👨‍🍳¿Qué te puedo ofrecer? 
-            show_menu(menu)",
+        "content": "👨‍🍳¿Qué te puedo ofrecer? show_menu(menu)",
     },
 ]
 

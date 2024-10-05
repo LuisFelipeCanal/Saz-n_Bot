e31414682e80d90c43fb5d2424469af745a0d84c
KimberlySalazarB <118402135+KimberlySalazarB@users.noreply.github.com>
2024-10-05 01:32:48 -0500

    Update main3.py

--- main3.py
+++ main3.py
@@ -124,8 +124,8 @@ def get_system_prompt(menu, distritos):
     El pedido confirmado será:\n
     {display_confirmed_order([{'Plato': '', 'Cantidad': 0, 'Precio Total': 0}])}\n
     Recuerda verificar que el pedido sea correcto antes de registrarlo.
-    Retorna pedido que es una variable que contiene los platos y cantidades y precio total en un json del pedido confirmado
-    {get_order_json([pedido])}
+    pedido es igual a los platos y cantidades y precio total en un json del pedido confirmado
+    {get_order_json(['pedido'])}
     """
     return system_prompt.replace("\n", " ")
 

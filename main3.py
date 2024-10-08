306c87ecd7d8846e1f3cb44f11e92cbe3156dfb5
KimberlySalazarB <118402135+KimberlySalazarB@users.noreply.github.com>
2024-10-08 02:18:45 -0500

    Update main3.py

--- main3.py
+++ main3.py
@@ -137,7 +137,7 @@ def get_system_prompt(menu, distritos):
     Si el cliente agrega postres o bebidas, incorpóralos en la tabla de resumen como un plato adicional y calcula el monto total nuevamente con precisión.
     
     Al final, pregúntale al cliente: "¿Estás de acuerdo con el pedido?" y espera su confirmación. 
-    Después, de que el cliente confirme el pedido, pide al cliente el metodo de pago (tarjeta, efectivo u otra opción disponible). Verifica que haya ingresado un metodo de pago por parte del cliente antes de continuar.
+    Después, de que el cliente confirme el pedido, pide al cliente el metodo de pago (tarjeta, efectivo o yape ). Verifica que haya ingresado un metodo de pago por parte del cliente antes de continuar.
     Luego, de verificar el metodo de pago, registra la hora actual de Perú como el timestamp {hora_lima} de la confirmación. 
      
     El pedido confirmado será:\n
